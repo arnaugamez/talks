c0be03c17f52dad67f9a831043194b446f42b726
Arnau <arnaugamez@protonmail.com>
2020-01-29 17:32:49 +0100

    Update install.sh

--- 2020/00_h-c0n/install.sh
+++ 2020/00_h-c0n/install.sh
@@ -4,7 +4,7 @@ sudo apt -y install git build-essential
 
 # Clone and install radare2
 git clone --depth=1 https://github.com/radareorg/radare2
-~/radare2/sys/install.sh
+./radare2/sys/install.sh
 
 # Install required dependencies for r2frida
 sudo apt -y install libzip-dev nodejs npm curl pkg-config
@@ -16,4 +16,4 @@ r2pm -ci r2dec
 
 # Download Cutter and give exec perm
 wget https://github.com/radareorg/cutter/releases/download/v1.10.0/Cutter-v1.10.0-x64.Linux.AppImage
-chmod +x ~/Cutter-v1.10.0-x64.Linux.AppImage
+chmod +x ./Cutter-v1.10.0-x64.Linux.AppImage
